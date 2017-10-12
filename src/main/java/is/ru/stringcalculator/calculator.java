b244687913ffb0a7932ec6fac3a31ac327d36d88
Goði Már Daðason <Godi@Gois-Air.lan>
2017-10-12 23:34:11 +0000

    Liður 1

--- src/main/java/is/ru/stringcalculator/calculator.java
+++ src/main/java/is/ru/stringcalculator/calculator.java
@@ -3,8 +3,27 @@ package is.ru.stringcalculator;
 public class Calculator{
 
   public static int add(String text){
-    return 0;
-
+    if(text.equals("")){
+        return 0;
+    }
+    else{
+        if(text.contains(",")){
+          String numbers[] = text.split(",");
+          return sum(numbers);
+        }
+        return 1;
+    }
   }
 
+  private static int toInt(String text){
+    return Integer.parseInt(text);
+  }
+
+  private static int sum(String[] numbers){
+    int total = 0;
+    for(String number : numbers){
+         total += toInt(number);
+    }
+    return total;
+  }
 }
